0fcb9d4a38a644e37daaf4eae62a3e4b24e93983
Corban Chiu <cpc75@cornell.edu>
2023-05-10 03:37:46 -0400

    removed routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { useState } from 'react'
 import './index.css'
 import NavBar from './components/NavBar'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <NavBar />
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-
+      <Home />
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import "./NavBar.css"
 
 function NavBar() {
@@ -8,31 +7,19 @@ function NavBar() {
       <head>
         <link href='https://fonts.googleapis.com/css?family=Space+Mono' rel='stylesheet'></link>
       </head>
-      <Link to="/" className="site-title">Corban Chiu</Link>
+      <div className="site-title">Corban Chiu</div>
       <ul>
-        {/* <PageLink to="/About">About</PageLink> */}
-        {/* <PageLink to="/Projects">Projects</PageLink> */}
         <li>
-          <Link to="https://drive.google.com/file/d/1h5JyJ79adEhlFB78jA4Eq7OWf3uw1HQw/view?usp=share_link" target="_blank">Resume</Link>
-        </li>
-        {/* <PageLink to="/Resume">Resume</PageLink> */}
-        {/* <PageLink to="/Contact">Contact</PageLink> */}
-        <li className='img-logos'>
-          <Link to="https://github.com/CorbanChiu" target="_blank"><img src={"GitHubLogo.png"} width={23}></img></Link>
+          <a href="https://drive.google.com/file/d/1h5JyJ79adEhlFB78jA4Eq7OWf3uw1HQw/view?usp=share_link" target="_blank">Resume</a>
         </li>
         <li className='img-logos'>
-          <Link to="https://www.linkedin.com/in/corban-chiu-90a655209/" target="_blank"><img src={"LinkedInLogo.png"} width={23}></img></Link>
+          <a href="https://github.com/CorbanChiu" target="_blank"><img src={"GitHubLogo.png"} width={23}></img></a>
+        </li>
+        <li className='img-logos'>
+          <a href="https://www.linkedin.com/in/corban-chiu-90a655209/" target="_blank"><img src={"LinkedInLogo.png"} width={23}></img></a>
         </li>
       </ul>
     </nav >
-  )
-}
-
-function PageLink({ to, children, ...props }) {
-  return (
-    <li>
-      <Link to={to} {...props}>{children}</Link>
-    </li>
   )
 }
 
